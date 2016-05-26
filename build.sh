082298cb7ff16fc635d504ba3a9e89afa56d9456
Franck Dehay <franck.dehay@orange.com>
2016-05-26 12:18:30 +0200

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
-cd bosh-repos
+cd bosh-repo
+su ubuntu -c "gem install bundle"
 su ubuntu -c "bundle install --local"
 su ubuntu -c "bundle exec rake stemcell:build_os_image[ubuntu,trusty,$PWD/tmp/ubuntu_base_image.tgz]"
