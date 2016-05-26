2d2bacfd4ccad72282b94f0e710f3c17da14a024
Franck Dehay <franck.dehay@orange.com>
2016-05-26 12:14:26 +0200

    changed folder

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-ls
-cd ../bosh-repos
+
+cd bosh-repos
 su ubuntu -c "bundle install --local"
 su ubuntu -c "bundle exec rake stemcell:build_os_image[ubuntu,trusty,$PWD/tmp/ubuntu_base_image.tgz]"
