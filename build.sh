6a9e29226b34098fe1f63271aed8f818b2f2c089
Franck Dehay <franck.dehay@orange.com>
2016-05-26 14:43:40 +0200

    remove ubuntu user usage

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
 cd bosh-repo
-su ubuntu -c "gem install bundle"
-su ubuntu -c "bundle install --local"
-su ubuntu -c "bundle exec rake stemcell:build_os_image[ubuntu,trusty,$PWD/tmp/ubuntu_base_image.tgz]"
+bundle install --local
+bundle exec rake stemcell:build_os_image[ubuntu,trusty,$PWD/tmp/ubuntu_base_image.tgz]
